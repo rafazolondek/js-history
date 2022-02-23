1c7e651f4aa81f580e95003a371d856b83944054
rafaz <rafaelzolondek@outlook.com>
2022-02-23 10:26:49 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function appear(){
+    document.getElementById("hidden").innerHTML = "<strong>JavaScript</strong> (frequentemente abreviado como JS) é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e, funcional). Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web";
+    document.getElementById("arrow").src = "images/caret-up.svg";
+}
+
